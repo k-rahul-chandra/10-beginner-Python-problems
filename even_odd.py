32ccdbf799c8c3fc2c04f8764b7e98cf9aedaf48
rahul <rahulchandrakarupakala@gmail.com>
2026-05-12 20:22:57 +0530

    done with even odd problem

--- even_odd.py
+++ even_odd.py
@@ -1,3 +1,3 @@
 #finding if a number is even or odd
 num = int(input("Enter a number: "))
-print("number is even") if (a%2)==0 else print("Odd")
+print("number is even") if (num%2)==0 else print("Odd")
